688c7ed5f6f1147f4203680af6968be8b538d9f4
BLACKAFROROSE <55460989+BLACKAFROROSE@users.noreply.github.com>
2020-04-17 05:22:18 +0200

    ddcddddd

---
+++ app/src/main/java/com/mulengabwalyajeff/myapplicationt4ttt/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mulengabwalyajeff.myapplicationt4ttt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void me(){
+
+    }
+}
